819604711a7a6a0532699a9e8d1a6dd86fee9f4f
preddybreddy <p7.ready@gmail.com>
2024-01-06 19:49:53 -0500

    Done with css-animations on homepage

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,15 @@
+import './index.scss'
+
+export default function AnimatedLetters({letterClass, strArray, idx}) {
+    return (
+        <span>
+            {
+                strArray.map((char, i) => {
+                    return (<span key={char+i} className={`${letterClass} _${i + idx}`}>
+                        {char}
+                    </span>)
+                })
+            }
+        </span>
+    )
+}
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,33 @@
 import './index.scss'
 import Logo_p from '../../assets/images/icons8-letter-p-50.png'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+
 export default function Home() {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    
+    const nameArr = ['r', 'a', 'n', 'a', 'y']
+    const jobTitleArr = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+    
+    useEffect(() => {
+     setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
     return (
         <div className='container home-page'>
             <div className='text-zone'>
-                <h1>Hi, <br/> I'm
+                <h1><span className={`${letterClass} _1`}>H</span>
+                    <span className={`${letterClass} _2`}>i</span>
+                    <span className={`${letterClass} _3`}>,</span>
+                    <br/>
+                    <span className={`${letterClass} _4`}>I</span>
+                    <span className={`${letterClass} _5`}>'</span>
+                    <span className={`${letterClass} _6`}>m</span>
                     <img src={Logo_p} alt="developer" />
-                    ranay
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArr} idx={15} />
                     <br />
-                    Software Developer
+                    <AnimatedLetters letterClass={letterClass} strArray={jobTitleArr} idx={20} />
                 </h1>
                 <h2>Full-stack developer / Software Architect</h2>
             </div>
